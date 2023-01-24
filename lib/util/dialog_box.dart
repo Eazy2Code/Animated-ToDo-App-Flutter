12d4b9f3aa66276ff91850c467618b0746275c28
Eazy2Code <eazy2code786@gmail.com>
2023-01-24 15:09:39 +0300

    #2 animated todo app

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+  DialogBox(
+      {super.key,
+      required this.controller,
+      required this.onSave,
+      required this.onCancel});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Color.fromARGB(89, 0, 0, 0),
+      content: Container(
+        height: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 3, color: Colors.black)),
+                labelText: "Enter your Task",
+                labelStyle: TextStyle(color: Colors.white),
+                hintText: "Add Task",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyButton(
+                  text: "Add",
+                  onPressed: onSave,
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                MyButton(
+                  text: "Cancel",
+                  onPressed: onCancel,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
